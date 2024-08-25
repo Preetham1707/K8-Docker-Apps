67eec43836e6ee515d4791bab5ed3631f5e812d2
Preetham1707 <reddypreetham85@gmail.com>
2024-08-25 21:41:22 +0000

    Troubleshooting frontend Attendence

--- AttendenceApp/Backend/index.js
+++ AttendenceApp/Backend/index.js
@@ -12,7 +12,7 @@ const app= express();
 const axios= require('axios')
 
 app.use(cors());
-app.use(cors({ origin: process.env.FRONTEND }));
+//app.use(cors({ origin: process.env.FRONTEND }));
 
 
 app.use(express.json());
